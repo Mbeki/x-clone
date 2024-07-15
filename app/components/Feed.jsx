1fd5e02003b151c792c31730e795f5514786b215
Nixon Mbeki <nwokila@gmail.com>
2024-07-15 22:46:13 +0300

    create feed component and its header

---
+++ app/components/Feed.jsx
@@ -0,0 +1,15 @@
+import { HiOutlineSparkles } from 'react-icons/hi';
+function Feed() {
+  return (
+    <div className='max-w-xl flex-grow border-x border-gray-200 sm:ml-[73px] xl:ml-[370px] xl:min-w-[576px]'>
+      <div className='sticky top-0 z-50 flex border-b border-gray-200 bg-white px-3 py-2'>
+        <h2 className='cursor-pointer text-lg font-bold sm:text-xl'>Home</h2>
+        <div className='hover-effect ml-auto flex h-9 w-9 items-center justify-center px-0'>
+          <HiOutlineSparkles size={20} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 import Image from 'next/image';
+import Feed from './components/Feed';
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Feed />
+    </>
+  );
 }
